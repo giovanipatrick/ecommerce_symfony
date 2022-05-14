13fbe3d3ddf2d0cdbd0c233dcbc8aaad1b72803d
Giovani Patrick <giovani.ks@hotmail.com>
2022-05-14 16:12:39 -0300

    Estrutura do Projeto

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,162 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Exception;
+use stdClass;
+
+
+/** 
+ * Classe responsável pela manipulação dos usuários
+ * @author Giovani Patrick <giovani@diminua.me>
+ * @package Ecommerce
+ * @version 1.0
+ */
+class UsuarioController extends AbstractController{
+
+    /** 
+     * Trata os dados e gera uma Exception caso os critérios não sejam atendidos
+     * @param Request $request
+     * @param boolean $update True para dados específicos a rota Update
+     * @throws Exception
+     * @return void
+     */
+    private function validateCreateAndUpdateParams($request,$update)
+    {
+        if($update){
+            if(!$request->get('id')){
+                throw new Exception('O identificar do usuário não foi informado!');
+            }
+        }
+
+        if(!$request->get('nome') || strlen($request->get('nome')) > 200){
+            throw new Exception('O nome não pode estar em branco e deve possuir no máximo 200 caracteres!');
+        }
+
+        if(!$request->get('sobrenome') || strlen($request->get('sobrenome')) > 200){
+            throw new Exception('O sobrenome não pode estar em branco e deve possuir no máximo 200 caracteres!');
+        }
+
+        if(!$request->get('email') || strlen($request->get('email')) > 200){
+            throw new Exception('O email não pode estar em branco e deve possuir no máximo 200 caracteres!');
+        }
+
+        if(!$update){
+            if(!$request->get('password') || strlen($request->get('password')) < 8 || strlen($request->get('password')) > 30){
+                throw new Exception('A senha deve possuir no minimo 8 caracteres e no máximo 30');
+            }
+        }
+
+        if(!intval($request->get('grupo'))){
+            throw new Exception('O grupo de permissões não foi informado!');
+        }
+
+        return;
+    }
+
+    /** 
+     * Cria um objeto com os dados enviados no Body da requisição e o retorna
+     * @param Request $request 
+     * @return stdClass
+     */
+    private function reqParams($request)
+    {
+        $data = new stdClass;
+        $data->id = intval($request->get('id'));
+        $data->nome = filter_var($request->get('nome'),FILTER_SANITIZE_ADD_SLASHES);
+        $data->sobrenome = filter_var($request->get('sobrenome',FILTER_SANITIZE_ADD_SLASHES));
+        $data->email = filter_var($request->get('email'),FILTER_SANITIZE_EMAIL);
+        $data->password = filter_var(md5($request->get('password')),FILTER_SANITIZE_ADD_SLASHES);
+        $data->grupo = filter_var($request->get('grupo'),FILTER_SANITIZE_ADD_SLASHES);
+        return $data;
+    }
+
+    /**
+     * Autenticação do Usuário
+     * @Route("/usuario/authenticate", name="app_usuario_authenticate", methods="POST")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function authUser(Request $request) : Response
+    {
+        return $this->json(
+            array("code"=>200,"type"=>"success","message"=>"Hello World"),
+        200);
+    }
+
+    /**
+     * Criar Usuário
+     * @Route("/usuario/create", name="app_usuario_create", methods="POST")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function createUser(Request $request) : Response
+    {
+        try{
+            $this->validateCreateAndUpdateParams($request,false);
+            $values = $this->reqParams($request);
+            return $this->json(array("code"=>200,"type"=>"success","message"=>$values));
+        }catch(Exception $e){
+            return $this->json(array("code"=>400,"type"=>"error","message"=>$e->getMessage()));
+        }
+    }
+
+    /** 
+     * Atualizar Usuário
+     * @Route("/usuario/update", name="app_usuario_update", methods="PUT")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function updateUser(Request $request) : Response
+    {
+        try{
+            $this->validateCreateAndUpdateParams($request,true);
+            $values = $this->reqParams($request);
+            return $this->json(array("code"=>200,"type"=>"success","message"=>$values));
+        }catch(Exception $e){
+            return $this->json(array("code"=>400,"type"=>"error","message"=>$e->getMessage()));
+        }
+    }
+
+    /** 
+     * Desabilitar Usuário
+     * @Route("/usuario/inactivate", name="app_usuario_inactivate", methods="DELETE")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function inactivateUser(Request $request) : Response
+    {
+    }
+
+    /** 
+     * Listar informações do Usuário
+     * @Route("/usuario/informations/{id}", name="app_usuario_informations", methods="GET")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function getUserInformations($id = null, Request $request) : Response
+    {
+        if(intval($id)){
+
+        }else{
+            return $this->json(
+                array("code"=>"400","type"=>"error","message"=>"O identificar do usuário não foi informado"),
+            400);
+        }
+    }
+
+    /** 
+     * Listar todos os usuários
+     * @Route("/usuario/listUsers", name="app_usuario_all", methods="GET")
+     * @param Request $request
+     * @return string JSON
+     */
+    public function getAllUsers(Request $request) : Response
+    {
+
+    }
+}
